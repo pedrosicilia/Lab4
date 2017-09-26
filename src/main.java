e5b09d9b9ff6ce807b9ae4f93771b8997903c27d
Pedrosicilia <pedro@siblaz.com>
2017-09-26 17:45:19 -0400

    This program will ask you to input a number, and if the randomly genereated number is the same as the inputed number, then it will say the numbers are the same.

---
+++ src/main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class main {
+    public static void main(String args[]) {
+
+        int usernum = 0;
+        int num = 0;
+
+
+        Random rand = new Random();
+        Scanner myscanner = new Scanner(System.in);
+
+        System.out.println("PICK A WHOlE NUMBER BETWEEEN 0 and 10!");
+        usernum = myscanner.nextInt();
+
+        num = rand.nextInt(10)+1;
+
+        if(num == usernum) {
+            System.out.println("WOW, we both picked: " +num+ " !!");
+        }
+
+        else
+        {
+            System.out.println("AWW, I picked " +num+ " but you picked " +usernum+ " ");
+        }
+
+
+
+    }
+
+
+}
